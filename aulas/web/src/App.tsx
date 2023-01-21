f47a5192e212cd940f839e6a23a33f37730585a0
Renan Fagundes <renan@gmail.com>
2023-01-21 17:56:42 -0300

    partindo pro frontEnd e configurando o header

--- aulas/web/src/App.tsx
+++ aulas/web/src/App.tsx
@@ -1,15 +1,24 @@
 import "./style/global.css";
+import logoImage from "./assets/logo.svg"
 import { Habit } from "./components/Habit"
 
-function App() {
+export function App() {
   return (
-    <div>
-      <Habit completed={3} />
-      <Habit completed={10} />
-      <Habit completed={20} />
-      <Habit completed={30} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16"> 
+        
+        <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
+          <img src={logoImage} alt="Habits" />
+
+          <button
+            type="button"
+            className="border border-violet-500 font-semibold rounded-lg px-6 py-4"
+          >
+            Novo hábito
+          </button>
+        </div>
+      </div>
+        
     </div>
   )
 }
-
-export default App
