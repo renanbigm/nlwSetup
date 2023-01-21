9ab4c53261a7044fad0059bf26f6316d87173c36
Renan Fagundes <renan@gmail.com>
2023-01-21 18:04:36 -0300

    header finalizado e componentizado

--- aulas/web/src/App.tsx
+++ aulas/web/src/App.tsx
@@ -1,24 +1,14 @@
+import { Header } from "./components/Header";
 import "./style/global.css";
-import logoImage from "./assets/logo.svg"
-import { Habit } from "./components/Habit"
 
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16"> 
         
-        <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
-          <img src={logoImage} alt="Habits" />
+      <Header />
 
-          <button
-            type="button"
-            className="border border-violet-500 font-semibold rounded-lg px-6 py-4"
-          >
-            Novo hábito
-          </button>
-        </div>
-      </div>
-        
+      </div>  
     </div>
   )
 }
