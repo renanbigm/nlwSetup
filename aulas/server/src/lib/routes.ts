819ee3e853eddd587bcd2793fae82f579fcdc76d
Renan Fagundes <renan@gmail.com>
2023-01-21 16:46:32 -0300

    implementando zod para validação das requisições

--- aulas/server/src/lib/routes.ts
+++ aulas/server/src/lib/routes.ts
@@ -1,9 +1,27 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { prisma } from "./prisma";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const habits = await prisma .habit.findMany()
+  app.post('/habits', async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(
+        z.number().min(0).max(6)
+      ),
+    })
+
+    const { title, weekDays } = createHabitBody.parse(request.body);
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: new Date(),
+        weekDays : {
+          create: weekDays.map((day) => { week_day: day })
+        }
+      }
+    })
   
     return habits;
   });
